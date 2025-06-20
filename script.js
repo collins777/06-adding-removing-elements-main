10d78cf987964fda4f00ba7a0077a5480dc9b52f
Brandon Collins <brandoncollins@Brandons-MBP-2.attlocal.net>
2025-06-20 16:15:41 -0400

    Implement item addition functionality in the TO-DON'T list application

---
+++ script.js
@@ -0,0 +1,29 @@
+// Get the form element
+const form = document.querySelector("#form");
+
+// Get the input field
+const input = document.querySelector("#item-input");
+
+// Get the list where items will be added
+const list = document.querySelector("#list");
+
+// Listen for the form's submit event
+form.addEventListener("submit", function (event) {
+  // Prevent the page from reloading
+  event.preventDefault();
+
+  // Get the text the user typed
+  const itemText = input.value;
+
+  // Create a new list item element
+  const li = document.createElement("li");
+
+  // Set the text of the new list item
+  li.textContent = itemText;
+
+  // Add the new list item to the list
+  list.appendChild(li);
+
+  // Clear the input field for the next item
+  input.value = "";
+});
